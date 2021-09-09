87512b89c461cea108d399092154b99689907855
smtcn <info@smtcn.cc>
2021-09-09 23:43:38 +0800

    cms
    cms

--- app/theme/cache/1bb070277f05cb35a3d740b939ad02d3.php
+++
@@ -1,5 +0,0 @@
-<?php include_once $this->_include('inc/_header.php', __FILE__); ?>
-<div class="alert alert-success">
-    The page you have requested could not be found.
-</div>
-<?php include_once $this->_include('inc/_footer.php', __FILE__); ?>
--- app/theme/cache/b15a47ee17ca1af75069fdba760643e1.php
+++
@@ -1,4 +0,0 @@
-<script src="/dist/src/bootstrap.min.js"></script>
-<script src="/dist/src/all.min.js"></script>
-</body> 
-</html>
